b742f22b2930b0b1b91f81e7d311dc4231e2c4ee
JayZ <jianzhen@doyobi.com>
2021-10-10 01:11:41 +0800

    feat: added components

---
+++ components/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="bg-gray-100 p-6">
+    <h1 class="text-4xl text-center">Vue Component</h1>
+
+    <article class="space-y-4">
+
+    </article>
+  </main>
+</template>
+
+<script setup>
+</script>
---
+++ components/src/components/PropsComponent.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-center my-6 bg-white">
+    <p>This is quite literally a dead view</p>
+  </div>
+</template>
+
